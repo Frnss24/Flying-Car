b32b701c0cecdbefbae2a53a96ef6532fa493117
Frnss24 <sinagafrans061@gmail.com>
2024-11-01 10:35:57 +0700

    The Source Code

---
+++ Frame.java
@@ -0,0 +1,26 @@
+import java.io.File;
+
+import javax.sound.sampled.*;
+import javax.swing.*;
+
+
+public class Frame {
+
+
+    public static void main(String[]args) throws Exception{
+        JFrame frame = new JFrame("Angry Car");
+
+        frame.add(new Board());
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1024,548);
+        frame.setVisible(true);
+
+        AudioInputStream sample;
+        sample = AudioSystem.getAudioInputStream(new File("rap.wav"));
+
+        Clip clip = AudioSystem.getClip();
+        clip.open(sample);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+}
